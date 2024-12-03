8d00e4a183a94965f86eda7d85ffa7baac1e97a6
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-03 05:57:29 -0300

    aprendendo sobre componentes

---
+++ 3_projeto_vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <PrimeiroComponente/>
+</template>
+
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue' //Aqui ele ainda não está sendo usado, mas está disponível para o uso
+
+export default {
+    name: 'App',
+    components: {
+      PrimeiroComponente
+    }
+  }
+</script>
