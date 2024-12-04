0d67ae49b6a17b06ed84869616c0f28d18749d76
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-04 06:40:35 -0300

    aula hierarquia de componentes

--- 3_projeto_vue/src/App.vue
+++ 3_projeto_vue/src/App.vue
@@ -2,18 +2,21 @@
   <div>
     <PrimeiroComponente/>
     <LifeCycle/>
+    <Pessoas/>
   </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue' //Aqui ele ainda não está sendo usado, mas está disponível para o uso
 import LifeCycle from './components/LifeCycle.vue'
+import Pessoas from './components/Pessoas.vue'
 
 export default {
     name: 'App',
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoas
     }
   }
 </script>
---
+++ 3_projeto_vue/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da pessoa: {{nome}}</h2>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>CSS</li>
+            <li>Vue.JS</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default{
+        name:'Info'
+    }
+</script>
