a1e16bd17b3a6c29c49d6e0c29b95a921afbb2e2
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-04 04:04:29 -0300

    Life Cycle

--- 3_projeto_vue/src/App.vue
+++ 3_projeto_vue/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <PrimeiroComponente/>
+  <div>
+    <PrimeiroComponente/>
+    <LifeCycle/>
+  </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue' //Aqui ele ainda não está sendo usado, mas está disponível para o uso
+import LifeCycle from './components/LifeCycle.vue'
 
 export default {
     name: 'App',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     }
   }
 </script>
---
+++ 3_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+        nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(()=>{
+            this.nome = 'Bruno'
+        },2000)
+    }, 
+    mounted() {
+       setTimeout(()=>{
+            this.nome = 'João'
+        },4000)
+    }
+}
+</script>
