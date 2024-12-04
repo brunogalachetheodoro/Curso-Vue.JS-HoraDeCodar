80e9e00ffbe53985846b4c1e7f9c2eb526888dfe
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-04 09:12:04 -0300

    aula diretivas

--- 3_projeto_vue/src/components/Info.vue
+++ 3_projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
         <h2>Esta é a descrição da pessoa: {{nome}}</h2>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if='esta_trabalhando'>Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>JavaScript</li>
             <li>CSS</li>
             <li>Vue.JS</li>
         </ul>
+        <p v-show='mostrar_email'>Mande uma mensagem para: <strong>{{email}}</strong></p>
     </div>
 </template>
 <script>
     export default{
-        name:'Info'
+        name:'Info',
+        data(){
+            return{
+                esta_trabalhando: false,
+                mostrar_email:true ,
+                email:'bruno@email.com'
+            }
+        }
     }
 </script>
