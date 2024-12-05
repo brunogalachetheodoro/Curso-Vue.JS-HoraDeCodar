20252772c6646a99b76736d2d00904c31cd51318
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-05 05:48:58 -0300

    aula argumentos

--- 3_projeto_vue/src/components/Info.vue
+++ 3_projeto_vue/src/components/Info.vue
@@ -10,16 +10,23 @@
             <li>Vue.JS</li>
         </ul>
         <p v-show='mostrar_email'>Mande uma mensagem para: <strong>{{email}}</strong></p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture/>
     </div>
 </template>
 <script>
+import Picture from './Picture.vue'
     export default{
         name:'Info',
+        components:{
+            Picture
+        },
         data(){
             return{
                 esta_trabalhando: false,
                 mostrar_email:true ,
-                email:'bruno@email.com'
+                email:'bruno@email.com',
+                meu_link: '//www.globoesporte.com/saopaulo'
             }
         }
     }
---
+++ 3_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <img v-bind:src="avatar" :alt="descricao"> <!-- V-bind e : funcionam da mesma forma (os dois pontos é para ser mais resumido)-->
+</template>
+<script>
+
+export default {
+    name:'Picture',
+    data(){
+        return {
+            avatar: '/img/avatar.png',
+            descricao: 'Bruno Theodoro'
+        }
+    }    
+}
+
+</script>
