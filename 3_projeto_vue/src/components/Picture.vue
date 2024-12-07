eeb1106ef7607bf976e24e9e510b219ba78ac418
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-07 08:50:29 -0300

    estudo $emit

--- 3_projeto_vue/src/components/Picture.vue
+++ 3_projeto_vue/src/components/Picture.vue
@@ -1,16 +1,28 @@
 <template>
-    <img v-bind:src="avatar" :alt="descricao"> <!-- V-bind e : funcionam da mesma forma (os dois pontos é para ser mais resumido)-->
+    <div>
+        <img v-bind:src="avatar" :alt="descricao"> <!-- V-bind e : funcionam da mesma forma (os dois pontos é para ser mais resumido)-->
+        <MudarImagem @mudar-imagem='trocarImagem' />
+    </div>
 </template>
 <script>
+import MudarImagem from './MudarImagem.vue'
 
 export default {
     name:'Picture',
+    components:{
+        MudarImagem
+    },
     data(){
         return {
             avatar: '/img/avatar.png',
             descricao: 'Bruno Theodoro'
         }
-    }    
+    } ,
+    methods: {
+        trocarImagem() {
+            this.avatar = '/img/avatar2.png'
+        }
+    } 
 }
 
 </script>
