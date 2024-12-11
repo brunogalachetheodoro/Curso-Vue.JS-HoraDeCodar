31333741f4118d1f0a123a91d9e36ed4188fbc41
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-12-11 06:27:38 -0300

    Inserindo dados no banco

--- 4_make_your_burger/src/components/BurgerForm.vue
+++ 4_make_your_burger/src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Coponente de Mensagem</p>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit='createBurger'>
                 <div class="input-container">
                     <label for="nome">Nome do Cliente:</label>
                     <input type="text" id="nome" name="nome" v-model='nome' placeholder="Digite o seu nome">
@@ -58,11 +58,36 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+        },
+        async createBurger(e) {
+            e.preventDefault();
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                opcionais: Array.from(this.opcionais),
+                status: 'Solicitado'
+            }
+             
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch('http://localhost:3000/burgers', {
+                method: 'POST',
+                headers: {'Content-Type':'application/json'},
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            this.nome ='';
+            this.carne ='';
+            this.pao='';
+            this.opcionais='';
         }
     },
     mounted(){
         this.getIngredientes()
     }
+
 }
 </script>
 
@@ -132,4 +157,5 @@ export default {
         background-color: transparent;
         color: #222;
     }
+
 </style>
